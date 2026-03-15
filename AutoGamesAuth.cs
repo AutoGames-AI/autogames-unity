3f88290c48349115610aceb1afac951455c98886
AcrossImposible <woger.games.mob@gmail.com>
2026-03-15 16:31:49 +0300

    Update get token

--- AutoGamesAuth.cs
+++ AutoGamesAuth.cs
@@ -1,36 +1,35 @@
 using System;
-using System.Collections;
-using System.Text;
-//#if UNITY_WEBGL && !UNITY_EDITOR
+using System.Threading.Tasks;
+#if UNITY_WEBGL && !UNITY_EDITOR
 using System.Runtime.InteropServices;
-//#endif
+#endif
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.Networking;
 
 namespace QAI.LogIn
 {
-    // The GameObject name must be "AuthManager"; if you rename it, update index.html accordingly.
     public class AutoGamesAuth : MonoBehaviour
     {
         public static AutoGamesAuth Instance;
 
-        [HideInInspector] public UnityEvent<UserProfile> onLogin;
-        [HideInInspector] public UnityEvent onLogout;
-
+        [HideInInspector] public UnityEvent<UserProfile> onLogin = new();
+        [HideInInspector] public UnityEvent onLogout = new();
 
         public UserProfile UserProfile { get; private set; }
+
+        // Используем TaskCompletionSource для превращения callback-системы в awaitable
+        private TaskCompletionSource<string> _tokenRequestSource;
 
 #if UNITY_WEBGL && !UNITY_EDITOR
         [DllImport("__Internal")]
         private static extern void TriggerAutoGamesLogin();
+        
         [DllImport("__Internal")]
         private static extern void TriggerAutoGamesLogout();
-        
-#endif
 
         [DllImport("__Internal")]
-        private static extern string GetAccessTokenJS();
+        private static extern void RequestAccessTokenJS();
+#endif
 
         private void Awake()
         {
@@ -43,7 +42,6 @@ namespace QAI.LogIn
             TriggerAutoGamesLogin();
 #else
             Debug.Log("Login available only in WebGL build. Simulating...");
-            // You can simulate a test profile in the editor
             OnAuthStateChanged("{\"ID\":123,\"Email\":\"test@test.local\",\"Name\":\"Dev\"}");
 #endif
         }
@@ -58,24 +56,23 @@ namespace QAI.LogIn
 #endif
         }
 
-        // This method is called from JS: SendMessage('AuthManager','OnAuthStateChanged', payload);
-        // payload = JSON string of the profile or an empty string on logout
         public void OnAuthStateChanged(string payloadJson)
         {
             if (string.IsNullOrEmpty(payloadJson))
             {
-                Debug.Log("User logged out");
-                // clear local session
-                HandleLoggedOut();
+                UserProfile = null;
+                onLogout?.Invoke();
                 return;
             }
 
             try
             {
-                // For simplicity, we use JsonUtility. The SDK's JSON may contain fields not supported by JsonUtility.
                 UserProfile profile = JsonUtility.FromJson<UserProfile>(payloadJson);
-                Debug.Log($"User logged in: {profile.Email} (ID {profile.ID})");
-                HandleLoggedIn(profile);
+                UserProfile = profile;
+                onLogin?.Invoke(profile);
+
+                // Пример асинхронного получения после логина
+                _ = PrintTokenAsync();
             }
             catch (Exception e)
             {
@@ -83,101 +80,44 @@ namespace QAI.LogIn
             }
         }
 
-        private async void HandleLoggedIn(UserProfile profile)
+        // Метод, который вызывает JS через SendMessage
+        public void OnAccessTokenReceived(string token)
         {
-            await Awaitable.EndOfFrameAsync();
-
-            UserProfile = profile;
-
-            onLogin?.Invoke(profile);
-
-            PrintToken();
+            if (_tokenRequestSource != null)
+            {
+                _tokenRequestSource.TrySetResult(token);
+                _tokenRequestSource = null;
+            }
         }
 
-        private void HandleLoggedOut()
+        public async Task<string> GetAccessToken()
         {
-            onLogout?.Invoke();
-        }
-
-        public async Awaitable<string> GetAccessToken()
-        {
-            await RefreshTokenAsync(GetAccessTokenJS());
-
 #if UNITY_WEBGL && !UNITY_EDITOR
-            return GetAccessTokenJS();
+            // Если запрос уже идет, ждем его
+            if (_tokenRequestSource != null) return await _tokenRequestSource.Task;
+
+            _tokenRequestSource = new TaskCompletionSource<string>();
+            RequestAccessTokenJS();
+            
+            return await _tokenRequestSource.Task;
 #else
             return "В юньке его нет :(";
 #endif
         }
 
-
-        public async void PrintToken()
+        private async Task PrintTokenAsync()
         {
-            await Awaitable.WaitForSecondsAsync(1f);
-
-#if UNITY_WEBGL && !UNITY_EDITOR
-            string token = GetAccessTokenJS();
-            Debug.Log(token != null ? $"Access Token: {token}" : "Token not foundoken");
-#endif
+            string token = await GetAccessToken();
+            Debug.Log(!string.IsNullOrEmpty(token) ? $"Access Token: {token}" : "Token not found");
         }
 
 #if UNITY_EDITOR
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.H))
-            {
-                OnLoginButtonClicked();
-            }
-            if (Input.GetKeyDown(KeyCode.O))
-            {
-                OnLogoutButtonClicked();
-            }
+            if (Input.GetKeyDown(KeyCode.H)) OnLoginButtonClicked();
+            if (Input.GetKeyDown(KeyCode.O)) OnLogoutButtonClicked();
         }
 #endif
-
-        private const string RefreshUrl = "https://api.dev.autogames.app/sso/refresh-token";
-
-        // Класс для структуры запроса (если нужен JSON в теле)
-        [System.Serializable]
-        public class RefreshRequest
-        {
-            //public string refresh_token;
-        }
-
-        public async Awaitable RefreshTokenAsync(string oldToken)
-        {
-            var data = new RefreshRequest { };
-            string json = JsonUtility.ToJson(data);
-
-            using (UnityWebRequest request = new UnityWebRequest(RefreshUrl, "POST"))
-            {
-                byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
-                request.uploadHandler = new UploadHandlerRaw(bodyRaw);
-                request.downloadHandler = new DownloadHandlerBuffer();
-
-                request.SetRequestHeader("Content-Type", "application/json");
-                //request.SetRequestHeader("Authorization", "Bearer " + oldToken);
-
-                // В Unity можно дождаться окончания UnityWebRequestAsyncOperation с помощью await
-                var operation = request.SendWebRequest();
-
-                while (!operation.isDone)
-                {
-                    await Awaitable.NextFrameAsync(); // Ждем следующего кадра, чтобы не блокировать основной поток
-                }
-
-                if (request.result == UnityWebRequest.Result.Success)
-                {
-                    Debug.Log("Токен успешно обновлен: " + request.downloadHandler.text);
-                    // Тут парсишь новый токен и сохраняешь его
-                }
-                else
-                {
-                    Debug.LogError("Token refresh error: " + request.error);
-                }
-            }
-        }
-
     }
 
     [Serializable]
@@ -189,4 +129,3 @@ namespace QAI.LogIn
         public string SolanaAddress;
     }
 }
-
--- Plugins/WebGL/AutoGamesBridge.jslib
+++ Plugins/WebGL/AutoGamesBridge.jslib
@@ -2,30 +2,20 @@ mergeInto(LibraryManager.library, {
   TriggerAutoGamesLogin: function() {
     if (typeof window.TriggerAutoGamesLogin === 'function') {
       window.TriggerAutoGamesLogin();
-    } else {
-      console && console.warn && console.warn('TriggerAutoGamesLogin not found');
     }
   },
 
   TriggerAutoGamesLogout: function() {
     if (typeof window.TriggerAutoGamesLogout === 'function') {
       window.TriggerAutoGamesLogout();
-    } else if (window.AutoGamesSDK && window.AutoGamesSDK.logout) {
-      window.AutoGamesSDK.logout();
     }
   },
 
-  GetAccessTokenJS: function () {
-    var token = window.getAccessToken();
-
-    if (!token) {
-      return null;
+  RequestAccessTokenJS: function () {
+    if (typeof window.dispatchGetAccessToken === 'function') {
+        window.dispatchGetAccessToken();
+    } else {
+        console.error("dispatchGetAccessToken not found in index.html");
     }
-
-    var bufferSize = lengthBytesUTF8(token) + 1;
-    var buffer = _malloc(bufferSize);
-    stringToUTF8(token, buffer, bufferSize);
-    
-    return buffer;
   }
 });
